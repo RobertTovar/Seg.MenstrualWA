ac8f2b5f09f4d30e0f9aa76740e5686bbdd138b1
Roberto Tovar <robertotovar290501@gmail.com>
2022-07-12 15:37:03 -0500

    Pequeños arreglos
    Se arregló el texto que sale en todas las pantallas y tambien se agregó el logo de LABSOL

--- Laravel/resources/views/layouts/footer.blade.php
+++ Laravel/resources/views/layouts/footer.blade.php
@@ -5,10 +5,13 @@
         <a class="navbar-brand" href="https://www.uaz.edu.mx/">
             <img src="img/escudo_uaz.gif" alt="UAZ" height="70">
         </a>
-        <a class="navbar-brand" href="/">
+        <a class="navbar-brand" href="https://cozcyt.gob.mx/labsol_new/">
+            <img src="img/labsol_logo.png" alt="LABSOL" height="70">
+        </a>
+        <a class="navbar-brand">
             <img src="img/main_logo.jpeg" class="rounded-circle" alt="OPC" height="70">
         </a>
-        <a href="/" class="navbar-brand">
+        <a class="navbar-brand">
             <img src="img/lab_logo.jpeg" class="rounded-circle" alt="Laboratorio" height="70">
         </a>
         <a class="navbar-brand" href="http://www.uaslp.mx/">
